b3c1c2f14a6cf6f09b0225f7282ca5530047bb8c
tassio7 <108763536+Tasmac10@users.noreply.github.com>
2024-09-26 13:37:45 -0300

    Version1

--- src/org/psnbtech/entity/Bullet.java
+++ src/org/psnbtech/entity/Bullet.java
@@ -5,33 +5,14 @@ import java.awt.Graphics2D;
 import org.psnbtech.Game;
 import org.psnbtech.util.Vector2;
 
-/**
- * Represents a Bullet within the game world.
- * @author Brendan Jones
- *
- */
 public class Bullet extends Entity {
-	
-	/**
-	 * The magnitude of the velocity of a Bullet.
-	 */
+
 	private static final double VELOCITY_MAGNITUDE = 6.75;
-	
-	/**
-	 * The maximum number of cycles that a Bullet can exist.
-	 */
+
 	private static final int MAX_LIFESPAN = 60;
-	
-	/**
-	 * The number of cycles this Bullet has existed.
-	 */
+
 	private int lifespan;
 
-	/**
-	 * Creates a new Bullet instance.
-	 * @param owner The object that fired the bullet.
-	 * @param angle The direction of the Bullet.
-	 */
 	public Bullet(Entity owner, double angle) {
 		super(new Vector2(owner.position), new Vector2(angle).scale(VELOCITY_MAGNITUDE), 2.0, 0);
 		this.lifespan = MAX_LIFESPAN;
